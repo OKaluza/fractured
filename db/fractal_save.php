71b5eb490761ee51cd62308a768e07987f64bf38
Owen Kaluza <6336397+OKaluza@users.noreply.github.com>
2012-04-15 23:34:15 +1000

    Better hash ids

--- db/fractal_save.php
+++ db/fractal_save.php
@@ -2,6 +2,42 @@
   session_start();
   include("connect.php");
 
+  /* From: http://blog.kevburnsjr.com/php-unique-hash */
+  function base62($int) {
+    /* Ascii :                    0  9,         A  Z,         a  z     */
+    /* $chars = array_merge(range(48,57), range(65,90), range(97,122)) */
+    $chars = array(
+      0=>48,1=>49,2=>50,3=>51,4=>52,5=>53,6=>54,7=>55,8=>56,9=>57,10=>65,
+      11=>66,12=>67,13=>68,14=>69,15=>70,16=>71,17=>72,18=>73,19=>74,20=>75,
+      21=>76,22=>77,23=>78,24=>79,25=>80,26=>81,27=>82,28=>83,29=>84,30=>85,
+      31=>86,32=>87,33=>88,34=>89,35=>90,36=>97,37=>98,38=>99,39=>100,40=>101,
+      41=>102,42=>103,43=>104,44=>105,45=>106,46=>107,47=>108,48=>109,49=>110,
+      50=>111,51=>112,52=>113,53=>114,54=>115,55=>116,56=>117,57=>118,58=>119,
+      59=>120,60=>121,61=>122
+    );
+ 
+    $key = "";
+    while($int > 0) {
+      $mod = $int-(floor($int/62)*62);
+      $key .= chr($chars[$mod]);
+      $int = floor($int/62);
+    }
+    return strrev($key);
+  }
+
+  /* From: http://blog.kevburnsjr.com/php-unique-hash */
+  function udihash($num, $len = 5) {
+    /* Next prime greater than 62 ^ n / 1.618033988749894848 */
+    #$golden_primes = array(1,41,2377,147299,9132313,566201239,35104476161,2176477521929,134941606358731);
+    /* Here's another set using the following primes */
+    $golden_primes = array(2,43,2381,147311,9132317,566201243,35104476163,2176477521961,13494160635873);
+    $ceil = pow(62, $len);
+    $prime = $golden_primes[$len];
+    $dec = ($num * $prime)-floor($num * $prime/$ceil)*$ceil;
+    $hash = base62($dec);
+    return str_pad($hash, $len, "0", STR_PAD_LEFT);
+  }
+
   $user = $_SESSION["user_id"];
   $goto = $_SERVER['HTTP_REFERER'];
   $desc = $_POST["description"];
@@ -29,14 +65,19 @@
   for ($count=0; $count<100; $count++) 
   {
     $ftime = microtime(true);
-    //base64_encode made url safe
-    $locator = rtrim(strtr(base64_encode(pack("d", $ftime)), '+/', '-_'), '=');
+    $inttime = intval($ftime * 1000);
+    //Create a 7 digit base62 hash
+    //Max = 3579346000000, /1000 = 3579346000 = runs out of digits on Fri, 04 Jun 2083
+    //Could change to /100 = 35793460000 = Sat, 02 Apr 3104, but more likely to get time collisions on inserts
+    $locator = udihash($inttime, 7);
 
     $query = "INSERT INTO fractal (locator, user_id, date, name, source, public, preview) values('$locator', '$user', '$mysqldate', '$desc', '$data', '$public', '$thumb');";
     $result = mysql_query($query);
     if ($result == 1) //Loop until insert successful
       break;
-    echo mysql_error();
+    //echo $ftime . "," . $inttime . "," . $locator . "<br>";
+    //echo mysql_error();
+    usleep(1000);  //Wait for 1 millisecond
   }
 
   mysql_close();
